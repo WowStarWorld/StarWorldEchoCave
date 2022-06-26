1308d3faa3055f02c8ece416f490818e9885b178
XieXiLin <xiexilin158@qq.com>
2022-06-26 17:43:03 +0800

    改进代码 & 修正错误

--- src/main/java/tk/starworldstudio/echocave.java
+++ src/main/java/tk/starworldstudio/echocave.java
@@ -69,19 +69,19 @@ public final class echocave extends JavaPlugin {
                 // 从getCaves()中随机取一个
                 int random = (int) (Math.random() * getCaves().size());
                 LinkedHashMap lmap = (LinkedHashMap) getCaves().get(random);
-                sender.sendMsg("\n§f│ §b回声洞 —— ("+random+")\n§f│ \n§f│ §e"+lmap.get("content").toString().replace("|", "\n§f│ &e").replace("&","§")+"\n"+"§f│ §b—— "+lmap.get("player").toString());
+                sender.sendMessage("\n§f│ §b回声洞 —— ("+random+")\n§f│ \n§f│ §e"+lmap.get("content").toString().replace("|", "\n§f│ &e").replace("&","§")+"\n"+"§f│ §b—— "+lmap.get("player").toString());
                 return true;
             }
             if (args.length == 1) {
                 if (args[0].equalsIgnoreCase("num")) {
-                    sender.sendMsg("§f│ §b回声洞 —— 回声洞数量（从0开始）："+(getCaves().size()-1));
+                    sender.sendMessage("§f│ §b回声洞 —— 回声洞数量（从0开始）："+(getCaves().size()-1));
                     return true;
                 }
             }
             else{
                 if (Objects.equals(args[0], "get")) {
                     LinkedHashMap lmap = (LinkedHashMap) getCaves().get(Integer.parseInt(args[1]));
-                    sender.sendMsg("\n§f│ §b回声洞 —— ("+args[1]+")\n§f│ \n§f│ §e"+lmap.get("content").toString().replace("|", "\n§f│ &e").replace("&","§")+"\n"+"§f│ §b—— "+lmap.get("player").toString());
+                    sender.sendMessage("\n§f│ §b回声洞 —— ("+args[1]+")\n§f│ \n§f│ §e"+lmap.get("content").toString().replace("|", "\n§f│ &e").replace("&","§")+"\n"+"§f│ §b—— "+lmap.get("player").toString());
                     return true;
                 }
                 if (Objects.equals(args[0], "add")) {
@@ -100,9 +100,9 @@ public final class echocave extends JavaPlugin {
                     Caves.add(lmap);
                     getConfig().set("caves", Caves);
                     saveConfig();
-                    sender.sendMsg("§f│ §b回声洞投稿成功！");
+                    sender.sendMessage("§f│ §b回声洞投稿成功！");
                     int size = Caves.size()-1;
-                    sender.sendMsg("§f│ §b使用/cave get "+size+" 查看");
+                    sender.sendMessage("§f│ §b使用/cave get "+size+" 查看");
                     broadcastMessage("§e新的回声洞! ("+size+")");
                     return true;
                 }
@@ -117,6 +117,7 @@ public final class echocave extends JavaPlugin {
                         getConfig().set("caves", Caves);
                         saveConfig();
                         sender.sendMessage("§f│ §b回声洞删除成功！");
+                        broadcastMessage("§e回声洞"+Integer.parseInt(args[1])+"被删除")
 
                     }else {
                         sender.sendMessage("§f│ §c你没有权限删除！");
